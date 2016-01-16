0e8031ab79011c9a6833e3b054b4315bb2bf71fb
Anthony Gore <anthony@magnum.technology>
2016-01-16 12:56:53 +1100

    Year select animation

--- nodeserver/src/public/js/main.js
+++ nodeserver/src/public/js/main.js
@@ -14,9 +14,7 @@ $(document).ready(function(){
             })
     };
 
-    var generateTimeLines = function(scale, year_start, year_end){
-
-        var inc = 5;
+    var generateTimeLines = function(scale, inc, year_start, year_end){
 
         for (var i = 0; i <= Math.floor(year_end - year_start)/inc; i++) {
 
@@ -31,7 +29,8 @@ $(document).ready(function(){
             // Time scale
             $('<div></div>', {
                 class : 'col',
-                style : "width:" + (5 * scale) + "px;"
+                style : "width:" + (5 * scale) + "px;",
+                'data-year' : year_end - (i * 5)
             })
                 .appendTo('#time-scale')
                 .append('<div class="vertical-text">' + (year_end - (i * 5)) + '</div>')
@@ -40,6 +39,7 @@ $(document).ready(function(){
 
         $('#time-scale').css('width', 2000 * scale);
         $('#time-lines').css('width', 2000 * scale);
+        $('#chart').css('width', 2000 * scale);
     };
 
     var generateRows = function(resp, year_start, year_end){
@@ -76,7 +76,8 @@ $(document).ready(function(){
         year_end = 2010,
         row_start = 1,
         row_end = 30,
-        scale = 20
+        scale = 20,
+        inc = 5
     ;
 
     // Create first row on load
@@ -90,7 +91,30 @@ $(document).ready(function(){
     getData(year_start, year_end, row_start, row_end, generateRows);
 
     // Generate time lines on load.
-    generateTimeLines(scale, year_start, year_end);
+    generateTimeLines(scale, inc, year_start, year_end);
 
+    // Populate select
+    var year_select = $('select', '#year-select');
+    for (var i = year_end; i >= year_start; i -= inc) {
+        year_select.append('<option value="' + i + '">' + i + '</option>');
+    }
+    year_select
+        .on('change', function(event, type){
+            if (typeof(type) === 'undefined') {
+                type = 'scroll'
+            }
+            var elem = $('#chart-wrapper');
+            var pos = (scale * this.value) - elem.width();
+            var speed = Math.min(Math.abs(elem.scrollLeft() - pos), 2000);
+            console.log(speed);
+            if (type == 'scroll') {
+                elem.animate({scrollLeft: pos}, speed);
+            } else {
+                elem.scrollLeft(pos);
+            }
+        })
+        .val(1980)
+        .trigger('change', 'jump')
+    ;
 
 });
